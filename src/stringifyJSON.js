2bcd70fff747c1e17ab12e40c741a6bb10a9bdb8
Sonny Sheth <sonny.sheth@gmail.com>
2015-12-04 13:11:21 -0200

    Complete stringifyJSON function using recursion to pass test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+
+  else if(typeof obj === 'number'){
+    return obj.toString();
+  }
+
+  else if(typeof obj === 'boolean'){
+    return obj.toString();
+  }
+
+  else if(typeof obj === 'function'){
+    return null;
+  }
+
+  else if(obj === null){
+    return "null";
+  }
+
+  else if(obj === undefined){
+    return null;
+  }
+
+  else if(Array.isArray(obj)){
+    return '[' +
+      obj.map(function(item) {
+        return stringifyJSON(item)
+      }).join(",") + ']';
+  }
+
+  else if((!!obj) && (obj.constructor === Object)){
+    var result = [];
+    var objKeys = Object.keys(obj);
+    objKeys.forEach(function(key){
+      var val = stringifyJSON(obj[key]);
+      if(val !== null){
+        result.push('"' + key + '":' + val);
+      }
+    });
+    return '{' + result.join(',') + '}';
+  }
+
 };
